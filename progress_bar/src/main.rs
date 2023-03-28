51c4928ff390b4621906aa1a89e3e00bd9aa1680
Ich-mag-dich <san02@kakao.com>
2023-03-28 12:22:18 +0900

    commit

--- progress_bar/src/main.rs
+++ progress_bar/src/main.rs
@@ -5,15 +5,14 @@ use std::{
 };
 
 fn main() {
+    println!("");
     let mut stdout = stdout();
     let mut bar = String::new();
-    let mut bar2 = String::new();
     for i in (0..=100).into_iter() {
-        bar = "█".repeat(i / 2);
-        bar2 = " ".repeat(50 - i / 2);
-        print!("\r\x1b[93m[{}{}] {i}%... \x1b[0m", &bar, &bar2);
+        bar = format!("{}{}", "█".repeat(i / 2), " ".repeat(50 - i / 2));
+        print!("\r\x1b[93m[{}] {i}%... \x1b[0m", &bar);
         stdout.flush().unwrap();
         sleep(Duration::from_millis(20));
     }
-    println!("\r\x1b[32m[{}{}] 100%       \x1b[0m", &bar, &bar2);
+    println!("\r\x1b[32m[{}] 100%       \x1b[0m\n", &bar);
 }
